dc8c4b031219e9627a8cbf03088ec545f62d2338
Thibaut G <tgourdel@users.noreply.github.com>
2020-10-15 14:59:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,15 @@ class MyTestClass(BaseCase):
 
     def test_basic(self):
         self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
+        self.open("https://talend.github.io/pendo-product-tracking-demo/")
